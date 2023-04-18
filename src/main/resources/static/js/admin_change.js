ef8886fcfb83d56304a299c2e7a1f3c0f28613ba
shimmer <cszshimmer@outlook.com>
2023-04-18 18:45:34 +0100

    update comment

--- src/main/resources/static/js/admin_change.js
+++ src/main/resources/static/js/admin_change.js
@@ -6,44 +6,64 @@ window.onload = function (ev) {
 };
 
 initNum = function () {
-    var total = document.getElementById('total_num');
-    var html_text = ''
-    // get commentlist
-    $.ajax({
-        url: "comment/comment_list",
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            html_text = '<span>' + data + '<span/>'
-            total.innerHTML = html_text;
-        }
-    });
+    // var total = document.getElementById('total_num');
+    // var html_text = ''
+    // // get commentlist
+    // $.ajax({
+    //     url: "comment/comment_list",
+    //     type: 'GET',
+    //     dataType: 'json',
+    //     success: function (data) {
+    //         html_text = '<span>' + data + '<span/>'
+    //         total.innerHTML = html_text;
+    //     }
+    // });
+    showNum()
 };
 
-showNum = function (num) {
+showNum = function () {
     var total = document.getElementById('total_num');
     var html_text = ''
-    html_text = '<span>' + num + '<span/>'
-    total.innerHTML = html_text;
+    $.ajax({
+        url: "admin_modify/0",
+        type: 'GET',
+        success: function (data) {
+            var num = data["productnum"]
+            html_text = '<span>' + "Remain:  "+num + '<span/>'
+            // alert(html_text);
+            total.innerHTML = html_text;
+        },
+        error: function (){
+            alert("error")
+        }
+    });
+
+
 }
 
 
 document.getElementById('change').onclick = function () {
 // function loginBtn(){
 // loginBtn.addEventListener("click", function() {
-    window.location.href = "admin_modify.html";
+//     window.location.href = "admin_modify.html";
     var num = document.getElementById("change_num").valueAsNumber;
-
 
     // ajax提交用户名+密码到后台程序
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "registerRecv.php");
+    xhr.open("get", "admin_modify/0/"+num);
     xhr.setRequestHeader("Content-type", "url");
-    xhr.send("admin_modify?productid=0&changed_num=" + num);
+    xhr.send("admin_modify/0/"+num);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var res = xhr.responseText;
-            showNum(JSON.parse(res))
+            var res_json = JSON.parse(res)
+            if (res_json["status"] === 1){
+                alert("Change success");
+                showNum()
+            }
+            else{
+                alert("change failed")
+            }
         }
     }
 }
--- src/main/resources/static/js/admin_login.js
+++ src/main/resources/static/js/admin_login.js
@@ -1,7 +1,6 @@
 // Login Form
 $(function () {
     var button = $('#loginButton');
-    loginBox
     var box = $('#loginBox');
     var form = $('#loginForm');
     button.removeAttr('href');
@@ -22,36 +21,110 @@ $(function () {
 
 
 // const loginBtn = document.getElementById("loginBtn");
-document.getElementById('loginBtn').onclick = function () {
-// function loginBtn(){
-// loginBtn.addEventListener("click", function() {
-    window.location.href = "admin_modify.html";
-    var input_username = document.getElementById("modlgn_username").value;
-    var input_password = document.getElementById("modlgn_password").value;
+// document.getElementById('loginBtn').onclick = function () {
+// // function loginBtn(){
+// // loginBtn.addEventListener("click", function() {
+//     window.location.href = "index.html";
+//     var input_username = document.getElementById("modlgn_username").value;
+//     var input_password = document.getElementById("modlgn_password").value;
+//
+//     // ajax提交用户名+密码到后台程序
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "http://localhost:8080/login?username=" + input_username + "&password=" + input_password);
+//     xhr.setRequestHeader("Content-type", "url");
+//     xhr.send("login?" + "username=" + input_username + "&password=" + input_password);
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             var res = xhr.responseText;
+//             // 后台响应成功则跳转到登录页
+//
+//             var res_json=JSON.parse(res)
+//
+//             if (res_json["status"] === 1) {
+//                 // alert("注册成功");
+//                 $.removeCookie('userid', {path: '/'})
+//                 $.cookie('userid', res.userid, {expires: 7, path: '/'});
+//                 window.location.href = "index.html";
+//             } else {
+//                 // print(res.type)
+//                 // print(res['status'])
+//                 // alert(typeof(res_str) +"  "+typeof (res_json)+"  "+typeof (res)+"@"+res+"@"+res_json["status"]+"@"+res_json2['status']+"@"+input_username+input_password);
+//                 alert("wrong user name or password");
+//             }
+//         }
+//     }
+// }
 
-    // ajax提交用户名+密码到后台程序
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "registerRecv.php");
-    xhr.setRequestHeader("Content-type", "url");
-    xhr.send("login?" + "username=" + input_username + "&password=" + input_password);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var res = xhr.responseText;
-            // 后台响应成功则跳转到登录页面
 
-            if (res.status === 1) {
-                // alert("注册成功");
-                $.removeCookie('adminuserid', {path: '/'})
-                $.cookie('adminuserid', res.userid, {expires: 7, path: '/'});
-                window.location.href = "admin_modify.html";
-            } else {
-                alert(res);
+// 监听表单提交事件
+// function submitform() {
+//     // window.location.href = "index.html";
+//     // alert("click")
+//     // 获取表单数据
+//     var input_username = document.getElementById('modlgn_username').value;
+//     var input_password = document.getElementById('modlgn_password').value;
+//     // alert(input_username+input_password)
+//     var domain = window.location.host;
+//     // alert(domain+" "+date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds())
+//     // 发送表单数据到服务器
+//     // 这里使用了假设的服务器URL和假设的POST请求
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "http://"+domain+"/login?username=" + input_username + "&password=" + input_password);
+//     xhr.setRequestHeader("Content-type", "");
+//     xhr.send("login?" + "username=" + input_username + "&password=" + input_password);
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             var res = xhr.responseText;
+//             // 后台响应成功则跳转到登录页
+//             var res_json=JSON.parse(res)
+//             if (res_json["status"] === 1) {
+//                 // alert(res)
+//                 // // alert("注册成功");
+//                 // $.removeCookie('userid', {path: '/'})
+//                 // $.cookie('userid', res.userid, {expires: 7, path: '/'});
+//                 window.location.href = "index.html";
+//             } else {
+//                 // print(res.type)
+//                 // print(res['status'])
+//                 // alert(typeof(res_str) +"  "+typeof (res_json)+"  "+typeof (res)+"@"+res+"@"+res_json["status"]+"@"+res_json2['status']+"@"+input_username+input_password);
+//                 alert("wrong user name or password");
+//             }
+//         }
+//     }
+//     return false;
+// }
 
-            }
+$("#submit_button").click(function () {
+    var domain = window.location.host;
+    // alert("yes")
+    document.getElementById("login-form").action = "http://"+domain+"/login"
+    $("#myiframe").one("load", function () {
+        var text = $(this).contents().find("body").text(); //获取到的是json的字符串
+        // var j = $.parseJSON(text);  //json字符串转换成json对象
+
+        var res_json=JSON.parse(text)
+        // alert(res_json["status"])
+        if (res_json["status"] === 1) {
+            // alert(res)
+            // // alert("注册成功");
+            // $.removeCookie('userid', {path: '/'})
+
+            $.cookie('username', res_json["username"], {expires: 7});
+            $.cookie('birthday', res_json["birthday"], {expires: 7});
+            $.cookie('userid', res_json["userid"], {expires: 7});
+            $.cookie('phone', res_json["phone"], {expires: 7});
+            $.cookie('email', res_json["email"], {expires: 7});
+            $.cookie('nickname', res_json["nickname"], {expires: 7});
+            window.location.href = "admin_modify.html";
+
+        } else {
+            // print(res.type)
+            // print(res['status'])
+            // alert(typeof(res_str) +"  "+typeof (res_json)+"  "+typeof (res)+"@"+res+"@"+res_json["status"]+"@"+res_json2['status']+"@"+input_username+input_password);
+            alert("wrong user name or password");
         }
-    }
-}
-
+    })
+});
 
 
 
