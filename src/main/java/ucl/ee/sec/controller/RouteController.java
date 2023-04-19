d05a70e94b21fcc186d7a2e34d0dd2daa9dcd324
JensenZhu <JensenZZZ@foxmail.com>
2023-04-19 21:42:25 +0100

    delete thymeleaf due to compatibility issues ande RouteController; back-end now don't support redirect, this function will be implemented in front end

--- src/main/java/ucl/ee/sec/controller/RouteController.java
+++ src/main/java/ucl/ee/sec/controller/RouteController.java
@@ -2,30 +2,36 @@ package ucl.ee.sec.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Deprecated
 @Controller
 @Slf4j
 public class RouteController {
-    @RequestMapping("/login")
+    @RequestMapping("/login_page")
     public String loginPage() {
         return "login";
     }
 
-    @RequestMapping("/comment")
+    @RequestMapping("/comment_page")
     public String commentPage() {
         return "comment";
     }
 
-    @RequestMapping("admin_modify")
+    @RequestMapping("admin_modify_page")
     public String adminModifyPage() {
         return "adminModify";
     }
 
-    @RequestMapping("/admin_login")
+    @RequestMapping("/admin_login_page")
     public String adminloginPage() {
 
         return "admin_login";
+    }
+
+    @RequestMapping({"/","index"})
+    public String indexPage(){
+        return  "index";
     }
 
 }
